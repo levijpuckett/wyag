f4d727d28a9351acfc6023049369c1ccc3eb2621
Levi Puckett <levijpuckett@gmail.com>
2023-06-16 23:09:23 -0700

    Update tests for readability, add verbosity.
    Also add tests for tagging and resolving refs.

--- python/test-wyag.sh
+++ python/test-wyag.sh
@@ -1,9 +1,32 @@
 #!/usr/bin/env bash
 set -e
 
+_V=0
+while getopts "v" opt; do
+    case $opt in
+        v)
+            _V=1
+            ;;
+    esac
+done
+function verbose() {
+    [[ $_V -eq 1 ]] && return 0 || return 1
+}
+verbose && echo "Verbose mode on"
+
 function step() {
-    pos=$(caller)
-    echo $pos $@
+    echo $(caller) $@
+}
+
+function run_test() {
+    cd left
+    git $@ > ../file1
+    verbose && git $@
+    cd ../right
+    $wyag $@ > ../file2
+    verbose && $wyag $@
+    cd ..
+    cmp file1 file2
 }
 
 wyag=$(realpath ./wyag)
@@ -18,24 +41,30 @@ fi
 cd $testdir
 echo "working in: $(pwd)"
 
-step Create repos
+step "Create repos"
 $wyag init left
 git init right > /dev/null
 
-step status
+# make sure user name and email lines up for tags and commits.
+cd right
+git config user.name Nobody
+git config user.email no@bo.dy
+cd ..
+
+step "status"
 cd left
 git status > /dev/null
 cd ../right
 git status > /dev/null
 cd ..
 
-step hash-object
+step "hash-object"
 echo "Don't read me" > README
 $wyag hash-object README > hash1
 git hash-object README > hash2
 cmp --quiet hash1 hash2
 
-step hash-object -w
+step "hash-object -w"
 cd left
 $wyag hash-object -w ../README > /dev/null
 cd ../right
@@ -44,23 +73,13 @@ cd ..
 ls left/.git/objects/b1/7df541639ec7814a9ad274e177d9f8da1eb951 > /dev/null
 ls right/.git/objects/b1/7df541639ec7814a9ad274e177d9f8da1eb951 > /dev/null
 
-step cat-file
-cd left
-$wyag cat-file blob b17d > ../file1
-cd ../right
-git cat-file blob b17d > ../file2
-cd ..
-cmp file1 file2
+step "cat-file with blob"
+run_test cat-file blob b17d
 
-step cat-file with long hash
-cd left
-$wyag cat-file blob b17df541639ec7814a9ad274e177d9f8da1eb951 > ../file1
-cd ../right
-git cat-file blob b17df541639ec7814a9ad274e177d9f8da1eb951 > ../file2
-cd ..
-cmp file1 file2
+step "cat-file with long hash"
+run_test cat-file blob b17df541639ec7814a9ad274e177d9f8da1eb951
 
-step "Create commit (git only, nothing is tested)" #@FIXME Add wyag commit
+step "Create commit (git only, nothing is tested)" #@FIXME Add wyag commit"
 cd left
 echo "Aleph" > hebraic-letter.txt
 git add hebraic-letter.txt
@@ -81,42 +100,21 @@ GIT_AUTHOR_DATE="2010-01-01 01:02:03 +0100" \
                GIT_COMMITTER_NAME="wyag-tests.sh" \
                GIT_COMMITTER_EMAIL="wyag@example.com" \
                git commit --no-gpg-sign -m "Initial commit" > /dev/null
-
 cd ..
 
-step cat-file on commit object without indirection
-cd left
-$wyag cat-file commit HEAD > ../file1
-cd ../right
-git cat-file commit HEAD > ../file2
-cd ..
-cmp file1 file2
+step "cat-file on commit object without indirection"
+run_test cat-file commit HEAD
 
-step cat-file on tree object redirected from commit
-cd left
-$wyag cat-file tree HEAD > ../file1
-cd ../right
-git cat-file tree HEAD > ../file2
-cd ..
-cmp file1 file2
+step "cat-file on tree object redirected from commit"
+run_test cat-file tree HEAD
 
-step cat-file with show_type on a commit
-cd left
-$wyag cat-file -t HEAD > ../file1
-cd ../right
-git cat-file -t HEAD > ../file2
-cd ..
-cmp file1 file2
+step "cat-file with show_type on a commit"
+run_test cat-file -t HEAD
 
-step cat-file with show_type on a blob
-cd left
-$wyag cat-file -t b17d > ../file1
-cd ../right
-git cat-file -t b17d > ../file2
-cd ..
-cmp file1 file2
+step "cat-file with show_type on a blob"
+run_test cat-file -t b17d
 
-step "Add some directories and commits (git only, nothing is tested)" #@FIXME Add wyag commit
+step "Add some directories and commits (git only, nothing is tested)" #@FIXME Add wyag commit"
 cd left
 mkdir a
 echo "Alpha" > a/greek_letters
@@ -145,41 +143,59 @@ GIT_AUTHOR_DATE="2010-01-01 01:02:03 +0100" \
                git commit --no-gpg-sign -m "Commit 2" > /dev/null
 cd ..
 
-step ls-tree
-cd left
-$wyag ls-tree HEAD > ../file1
-cd ../right
-git ls-tree HEAD > ../file2
-cd ..
-cmp file1 file2
+step "ls-tree"
+run_test ls-tree HEAD
 
-step checkout
+step "checkout"
 # Git and Wyag syntax are different here
 cd left
-$wyag checkout HEAD ../temp1
+$wyag checkout HEAD ../temp1 > /dev/null
 mkdir ../temp2
 cd  ../temp2
-git --git-dir=../right/.git checkout .
+git --git-dir=../right/.git checkout . > /dev/null
 cd ..
 diff -r temp1 temp2
 rm -rf temp1 temp2
 
-step rev-parse
+step "lightweight tag"
 cd left
-$wyag rev-parse HEAD  > ../file1
-$wyag rev-parse 75ee4 >> ../file1
-$wyag rev-parse 8a617  >> ../file1
-#@FIXME Tags missing, branches missing, remotes missing
+$wyag tag TEST
 cd ../right
-git rev-parse HEAD   > ../file2
-git rev-parse 75ee4 >> ../file2
-git rev-parse 8a617  >> ../file2
+git tag TEST
 cd ..
-cmp file1 file2
+run_test tag
 
-step "rev-parse (wyag redirection tester)"
+step "annotated tag"
+cd left
+$wyag tag -a TEST_ANNOTATED
+cd ../right
+git tag -a TEST_ANNOTATED -m 'blank'
+cd ..
+
+# confirms that the list of tags is the same.
+run_test tag 
+# confirms that the tag object is well structured (except the date!)
+run_test cat-file tag TEST_ANNOTATED | grep -v "tagger"
+
+step "rev-parse: named ref"
+run_test rev-parse HEAD
+
+step "rev-parse: short hash"
+run_test rev-parse 75ee4
+run_test rev-parse 8a61
+
+step "rev-parse: branch"
+run_test rev-parse main
+
+step "rev-parse: lightweight tag"
+run_test rev-parse TEST
+
+#step "rev-parse: annotated tag"
+#@FIXME wyag does not tag with date, so we can't compare rev-parse here!
+#step "rev-parse (wyag redirection tester)"
 #@TODO
 
-step THIS WAS A TRIUMPH
-step "I'M MAKING A NOTE HERE"
-step "HUGE SUCCESS"
+echo ""
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "All tests passing! Hooray!!"
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~"
